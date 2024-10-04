55e9dbc56af0c48adbedfe26e9f4d7f12ec37bba
fırat pıçak <firatozgurp@gmail.com>
2024-10-04 13:58:37 +0300

    Add project files.

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DoubleDeğişkenler
+            double number;
+            number = 4.85;
+            Console.WriteLine(number);
+
+            Console.WriteLine("***** Fiyat Listesi *****");
+            Console.WriteLine();
+            double applePrice, orangePrice, strawberryPrice, patato, tomato;
+
+
+            #endregion
+            Console.Read();
+
+        }
+    }
+}
